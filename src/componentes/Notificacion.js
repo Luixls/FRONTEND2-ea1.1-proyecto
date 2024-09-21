51914406c1ed4ec616b7f4282d21b22ffad79658
Luixls <56209449+Luixls@users.noreply.github.com>
2024-09-21 13:57:20 -0400

    Ajuste estilo notificación

--- src/componentes/Notificacion.js
+++ src/componentes/Notificacion.js
@@ -4,7 +4,7 @@ const Notificacion = ({ mensaje, tipo }) => {
   const color = tipo === 'error' ? 'bg-red-500' : 'bg-green-500';
 
   return (
-    <div className={`${color} text-white p-4 rounded`}>
+    <div className={`${color} text-white p-4 rounded shadow-lg fixed bottom-4 left-1/2 transform -translate-x-1/2 w-11/12 md:w-1/3 text-center`}>
       {mensaje}
     </div>
   );
